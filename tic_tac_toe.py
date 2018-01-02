6ee83385e0faaed0d277624bae58b5b24f01f494
nyccowgirl <nyc_cowgirl@hotmail.com>
2018-01-02 12:39:38 -0800

    update for formatting and refactoring

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,16 +1,17 @@
 import random
 
 
-# define function for greeting
-
 def greeting():
+    """Displays greeting to the game."""
+
     print "Let's play Tic Tac Toe!"
 
 
-# define function with instructions for position number for squares on board
-
 def instructions():
+    """Displays position number of the squares on the board."""
+
     separator = " | "
+
     print "\nThe following are the position numbers on the board:"
     print "\n  " + separator + " " + separator
     print " 1" + separator + "2" + separator + "3"
@@ -25,10 +26,11 @@ def instructions():
     print "  " + separator + " " + separator + "\n"
 
 
-# define function to draw board
-
 def draw_board(board):
+    """Displays the current board."""
+
     separator = " | "
+
     print "\n  " + separator + " " + separator
     print " " + board[1] + separator + board[2] + separator + board[3]
     print "  " + separator + " " + separator
@@ -42,11 +44,12 @@ def draw_board(board):
     print "  " + separator + " " + separator
 
 
-# define function for player to choose his/her letter (e.g., X or O)
-
 def player_letter():
-    # lets player choose letter and returns list with player's letter first and computer's letter last
+    """Lets player choose letter and returns list with player's letter first
+    and computer's letter last."""
+
     letter = ""
+
     while not (letter == "X" or letter == "O"):
         letter = raw_input("\nDo you want to be X or O? ")
         letter = letter.upper()
@@ -58,54 +61,60 @@ def player_letter():
             return ["O", "X"]
 
 
-# define function to determine who goes first based on random generator
-
 def who_first():
+    """Determines who goes first: player or computer."""
+
     if random.randint(0, 1) == 0:
         return "player"
     else:
         return "computer"
 
 
-# define function to determine if space is free
-
 def free_space(board, move):
+    """Determines if space is free for play."""
+
     return board[move] == " "
 
 
-# define function to let player input move
-
 def player_move(board):
+    """User input next move."""
+
+    positions = set([str(x) for x in (range(1, 10))])
     move = " "
-    while move not in "1 2 3 4 5 6 7 8 9".split() or not free_space(board, int(move)):
+
+    while move not in positions or not free_space(board, int(move)):
         move = raw_input("\nWhere would you like to go? (1-9) ")
-        if not move.isdigit() or (move < 1 or move > 9):
+
+        # Account for edge cases
+        if not move.isdigit() or (int(move) < 1 or int(move) > 9):
             print "Please input a position between 1 to 9."
         else:
             print "That space is taken; try again."
+
     return int(move)
 
 
-# define function to display the move on the board
-
 def make_move(board, letter, move):
+    """Makes the move on the board."""
+
     board[move] = letter
 
 
-# define function to create a copy of the board to determine possible computer move
-
 def copy_board(board):
-    copyboard = []
+    """Creates copy of board to determine possible moves for computer."""
+
+    copyboard = []  # deepcopy in copy library may be an option
 
     for square in board:
         copyboard.append(square)
     return copyboard
 
 
-# define function to make random move for computer
-
 def move_random(board, move_list):
+    """Makes random move for computer."""
+
     possible_moves = []
+
     for square in move_list:
         if free_space(board, square):
             possible_moves.append(square)
@@ -116,10 +125,8 @@ def move_random(board, move_list):
         return 0
 
 
-# define function to determine computer's move and display the move on the board
-
 def computer_move(board, player, computer):
-    # provide various scenarios to determine computer move
+    """Provide various scenarios to determine computer move."""
 
     # check if next move will result in win
     for square in range(1, 10):
@@ -150,51 +157,56 @@ def computer_move(board, player, computer):
     return move_random(board, [2, 4, 6, 8])
 
 
-# define function to determine if there is a winner
-
 def check_win(board, player, computer):
+    """Checks if there is a winner before advancing."""
+
     combinations = ([1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7])
+
     for com in combinations:
         if (board[com[0]] == player) and (board[com[1]] == player) and (board[com[2]] == player):
             return "player"
-        if (board[com[0]] == computer) and (board[com[1]] == computer) and (board[com[2]] == computer):
+        elif (board[com[0]] == computer) and (board[com[1]] == computer) and (board[com[2]] == computer):
             return "computer"
+
     return False
 
 
-# define function to check if board is full
-
 def full_board(board):
+    """Checks if board is full and out of moves."""
+
     for square in range(1, 10):
         if free_space(board, square):
             return False
+
     return True
 
 
-# define function to switch turns
-
 def switch_turn(turn):
+    """Switches player/computer turns."""
+
     if turn == "player":
         return "computer"
     else:
         return "player"
 
 
-# define function to allow player to play again
-
 def play_again():
+    """User input to play again."""
+
     play = raw_input("Do you want to play again? (Y/N) ")
     play = play.upper()
+
     if play not in ["Y", "N"]:
         print "Please input Y or N!"
     elif play == "Y":
         return True
 
 
-# define function to execute the game of Tic Tac Toe
-
 def execute_repl():
+    """Executes tic tac toe game."""
+
     greeting()
+
     while True:
         board = [" "] * 10
         instructions()
@@ -203,7 +215,7 @@ def execute_repl():
         print "The " + turn + " will go first."
 
         while True:
-            if turn = "player":
+            if turn == "player":
                 # Player's turn
                 draw_board(board)
                 move = player_move(board)
@@ -239,5 +251,8 @@ def execute_repl():
         if not play_again():
             break
 
+################################################################################
 
-execute_repl()
+if __name__ == '__main__':
+
+    execute_repl()
